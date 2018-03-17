07926c18442bd1bb2176e8d746c83d9e0cd8a443
Terry Wang <terrywang@tianyu.ninja>
2018-03-17 19:35:49 -0400

    UI2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
+const favicon = require('serve-favicon');
 
 
 var app = express();
@@ -9,13 +10,16 @@ app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/assets')));
 
+app.use(favicon(path.join(__dirname, '/assets', 'favicon.ico')))
+
 app.get('/', (req, res) => {
   var content = {
     tagList: ['Story', 'Date', 'Quote', 'Flirt'],
     tipList: [
       {
         title: 'Title #1',
-        content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec.'
+        content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec.',
+        tag: '#Story #Quote'
       },
       {
         title: 'Title #2',
@@ -36,11 +40,26 @@ app.get('/', (req, res) => {
       {
         title: 'Title #6',
         content: 'Content #6'
+      },
+      {
+        title: 'Title #7',
+        content: 'Content #7'
+      },
+      {
+        title: 'Title #8',
+        content: 'Content #8'
+      },
+      {
+        title: 'Title #9',
+        content: 'Content #9'
+      },
+      {
+        title: 'Title #10',
+        content: 'Content #10'
       }
     ]
   }
   res.render('index', content);
-  console.log('Received request: ' + req.originalUrl);
 })
 
 app.listen(3000);
